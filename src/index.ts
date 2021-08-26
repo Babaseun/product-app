391eef95880a0e757915e1f7557c8f927b993d8a
Adeyemi Onibokun <ababaseun@gmail.com>
2021-08-26 18:13:20 +0100

    Moved static assets

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import path from "path";
 import { productRouter } from "./routers/product.router";
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../client/product-app/build/")));
 
 app.use("/", productRouter);
 
